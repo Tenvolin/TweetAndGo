d8329e4c8b33a3adafc04d40d986885990b98d79
tenvolin <tenvolin@gmail.com>
2018-12-21 23:44:58 -0800

    Installed package manager:composer.

--- Manager.php
+++ Manager.php
@@ -9,14 +9,7 @@ include "DataParser.php";
 include "DataFetcher.php";
 include "util.php";
 
-include "libs/DiDom/ClassAttribute.php";
-include "libs/DiDom/Element.php";
-include "libs/DiDom/Errors.php";
-include "libs/DiDom/Query.php";
-include "libs/DiDom/Document.php";
-include "libs/DiDom/Encoder.php";
-include "libs/DiDom/StyleAttribute.php";
-include "libs/DiDom/Exceptions/InvalidSelectorException.php";
+include "vendor/autoload.php";
 
 $dataFetcher = new DataFetcher();
 $dataFetcher->fetch();
